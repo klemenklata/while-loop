0b84d91fa3f07d581d4f9259267c0234fdbf48a6
Clement Machado <cm@helloai.com>
2022-09-17 20:17:21 +0200

    upload files

---
+++ setup-git.sh
@@ -0,0 +1,7 @@
+echo "# while-loop" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/klemenklata/while-loop.git
+git push -u origin main
---
+++ wait-scripts.sh
@@ -0,0 +1,14 @@
+
+ i=$(curl http://127.0.0.1:8000/ | awk -F':' '{ print $2 }' | sed 's/}//')
+
+while [ $i -lt 2 ]
+do
+  echo "HDFS is restarting..."
+  sleep 3
+  i=$(curl http://127.0.0.1:8000/ | awk -F':' '{ print $2 }' | sed 's/}//')
+  if [[ "$i" == '2' ]]; then
+    break
+  fi
+done
+
+echo 'HDFS Started Successuflly!!'
